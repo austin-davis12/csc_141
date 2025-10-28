1cf4757d5e2477caf17ca0139aa011fc40108582
austin-davis12 <austin.davis001@albright.edu>
2025-10-28 11:20:57 -0400

    Delete assignments/04_10_slice.py

--- assignments/04_10_slice.py
+++
@@ -1,9 +0,0 @@
-Animals =[ '''Dog''', '''Falcon''', '''Horse''']
-print("The first 2 animals in this list are")
-print(Animals[0:2])
-
-print("The middle animal in this list is")
-print(Animals[1:2])
-
-print("The last 2 animals in this list are")
-print(Animals[1:3])

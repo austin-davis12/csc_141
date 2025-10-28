2c6eb1ba411ed0e1a45078c0997fe4a811d7c25b
austin-davis12 <austin.davis001@albright.edu>
2025-10-28 11:19:25 -0400

    Delete assignments/04_01_pizzas.py

--- assignments/04_01_pizzas.py
+++
@@ -1,4 +0,0 @@
-pizza = ['Pepperoni', 'Meat', 'Buffalo Chicken']
-for pizzas in pizza:
-    print(f"I like {pizzas.title()} pizza")
-print("Pizza is one of my favorite foods and I don't know what I would do without it. I really love pizza!")

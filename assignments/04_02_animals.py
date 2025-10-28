e18e4fcde5428df55b18096e5b27f38df735283a
austin-davis12 <austin.davis001@albright.edu>
2025-10-28 11:19:35 -0400

    Delete assignments/04_02_animals.py

--- assignments/04_02_animals.py
+++
@@ -1,4 +0,0 @@
-animals = ('Lion', 'Bear', 'Tiger')
-for animal in animals:
-    print(f"You would not want to run into a {animal.title()} in the wild")
-print("You don't want to see any of these animals when you are in the wild.")
